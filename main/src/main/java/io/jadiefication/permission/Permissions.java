79bffc8f1e72ce59f4b8a580de21bb4967d9bfce
Jadiefication <jadeczjade008@gmail.com>
2025-02-24 13:23:51 +0100

    feat: initialization of basic permissions

--- main/src/main/java/io/jadiefication/permission/Permissions.java
+++ main/src/main/java/io/jadiefication/permission/Permissions.java
@@ -12,7 +12,21 @@ public record Permissions(String name) {
         PERMISSIONS.add(this);
     }
 
-    public static boolean hasPermission(String name) {
+    public static void initialize() {
+        PERMISSIONS.addAll(List.of(new Permissions("OP"),
+                new Permissions("USE_CUSTOM_ITEM"),
+                new Permissions("PLACE"),
+                new Permissions("PARTICLE_SHAPE"),
+                new Permissions("TIME"),
+                new Permissions("WEATHER"),
+                new Permissions("GAMEMODE"),
+                new Permissions("STOP"),
+                new Permissions("START"),
+                new Permissions("BREAK")
+                ));
+    }
+
+    public static boolean exists(String name) {
         return PERMISSIONS.contains(new Permissions(name));
     }
 }
